fb7bfa72ae82b649f0b24d6c142556457acd3af4
luisescx <luis.esculapio@live.com>
2022-06-25 21:27:38 -0300

    feat: added populate route, services and controller

--- src/api/game/controllers/game.js
+++ src/api/game/controllers/game.js
@@ -1,9 +1,23 @@
-'use strict';
+"use strict";
 
 /**
  *  game controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::game.game');
+module.exports = createCoreController("api::game.game", ({ strapi }) => ({
+  populate: async (ctx) => {
+    console.log("Starting to populate");
+
+    const options = {
+      sort: "popularity",
+      page: "1",
+      ...ctx.query,
+    };
+
+    await strapi.service("api::game.game").populate(options);
+
+    ctx.send("Finished populating");
+  },
+}));
